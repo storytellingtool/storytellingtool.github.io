cfcd9d68c3932b87c0be844f4653d3b4a8946ffb
Qiaoliwang2020 <414217795zoe@gmail.com>
2024-12-19 20:43:12 +1100

    fix:add env client

---
+++ env-client.js
@@ -0,0 +1,6 @@
+self.env = {
+    "cmsUrl": "https://storytellingtool.github.io/",
+    "theme": "one",
+    "logosBaseUrl": "https://s3-ap-southeast-2.amazonaws.com/one-disease-storytelling-tool/",
+    "serviceWorkers": "true"
+  };
